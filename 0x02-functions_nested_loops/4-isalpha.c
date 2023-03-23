fab361e0f396ea2c03ce31c9af709518a01c6cdf
NIRUGIRA Muhizi Norbert <norbertmuhizi21@gmail.com>
2023-03-22 18:31:36 -0700

    checks for alphabetic character.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - checks for an alphabetic char
+ * @c: character to be checked
+ * Return: 1 if yes, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
