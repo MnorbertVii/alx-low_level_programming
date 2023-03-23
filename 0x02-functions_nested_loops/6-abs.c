4668ed32eb4139469a4dfbc5e91616c48b3cc51f
NIRUGIRA Muhizi Norbert <norbertmuhizi21@gmail.com>
2023-03-22 19:34:04 -0700

    prints the last digit of a number.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - calculates the absolute value of an integer
+ * @num: is the integer to be used
+ * Return: the absolute value integerr of a number
+ */
+int _abs(int num)
+{
+	if (num >= 0)
+	{
+		return (num);
+	}
+	return (-num);
+}
---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,18 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _putchar(char);
+int print_last_digit(int);
+int main(void)
+{
+    int r;
+
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
+}
