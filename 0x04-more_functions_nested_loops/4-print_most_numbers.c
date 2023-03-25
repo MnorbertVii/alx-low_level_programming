71e531de22613354b5ae0863118caf7c11fd885f
NIRUGIRA Muhizi Norbert <norbertmuhizi21@gmail.com>
2023-03-24 17:53:27 -0700

    function that prints the numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,10 +12,14 @@ void print_most_numbers(void)
 
 	for (c = 48; c < 58; c++)
 	{
-		if (c == 50 || c == 52)
-			continue;
-		else
-			_putchar(c);
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
+
 	}
 	_putchar("\n");
 }
