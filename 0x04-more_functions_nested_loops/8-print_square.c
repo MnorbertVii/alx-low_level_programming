7032742e6785e0d959ccae1de01fff701bb398da
NIRUGIRA Muhizi Norbert <norbertmuhizi21@gmail.com>
2023-03-24 19:47:50 -0700

    prints a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line.
+ * @size: size of the square
+ *
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int r, c;
+
+		for (r = 0; r < size; r++)
+		{
+			for (c = 0; c < size; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
